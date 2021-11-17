9fe3fe47825144ad1e1fea0f6d3c400ce8e70efa
“anildalar” <dollorinfotech@gmail.com>
2021-11-17 08:15:53 +0530

    part 2 variables

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+    echo '<h6>KrishnaPal</h6>';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Tutorials Part 3</title>
+        <style>
+            /* CSS Selector */
+              h1{ /* 1. Element Selector */
+                    color:red;
+              }  
+        </style>
+    </head>
+    <body>
+        <?php 
+            echo 'Welcome to PHP Training';
+            echo '<h1>Abhishek</h1>';
+            echo "<h2>Avinash</h2>";
+
+
+            $x = 10;
+
+            echo $X;
+        ?>
+        <script>
+            //JS Tag
+            document.querySelector('h2').style.color= 'green';
+        </script>
+    </body>
+</html>
+<?php
+    echo '<h3>Payal</h3>';
+?>
